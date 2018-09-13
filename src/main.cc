7b6fa074a3475ea04effd3d4c7a941fc4a4a4bf3
VC Science Labs <vscilab@vcccd.edu>
2018-09-13 09:46:32 -0700

    Good Luck!

---
+++ src/main.cc
@@ -0,0 +1,19 @@
+/**
+* CS V13 Object-Oriented Programming
+* CRN: [412]
+* Assignment: EX01-HelloWorld
+*
+* Statement of code ownership: I hereby state that I have written all of this
+* code and I have not copied this code from any other person or source.
+*
+* @author [Erik Crespo]
+*/
+
+#include <iostream>
+#include <csv13>
+using namespace std;
+
+int main() {
+	cout << "Hello, World!" << endl;
+	return 0;
+}
